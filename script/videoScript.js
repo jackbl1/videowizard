297028ab174c1424d304296f9e093beae79f0b99
jackbl1 <jackbloomfeld1@gmail.com>
2017-12-12 16:16:36 +0100

    Javascript video description

--- script/videoScript.js
+++ script/videoScript.js
@@ -1,6 +1,11 @@
 $(function(){
     $('a.read_more').click(function(event){
         event.preventDefault();
-        document.getElementById("more_text").style.display='block';
+        if(document.getElementById("more_text").style.display == 'none'){
+            document.getElementById("more_text").style.display = 'block';
+        }
+        else{
+            document.getElementById("more_text").style.display = 'none';
+        }
     });
 });
